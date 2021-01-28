f03d110c3eb010e1b28565acafdf5a4c908ced46
hidems <hidemiura.arms@gmail.com>
2021-01-28 21:15:11 +0100

    service with argument by makewith

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -7,11 +7,18 @@ use Illuminate\Http\Request;
 
 class ServiceController extends Controller
 {
-    public function my_service(MyService $myService)
+    // public function my_service(MyService $myService) // Make MyService instance by argument
+    public function my_service(int $id = -1)
     {
+        // Make MyService instance. which are same.
+        // $myService = app('App\MyClasses\MyService');
+        // $myService = app()->make('App\MyClasses\MyService');
+        // $myService = resolve('App\MyClasses\MyService');
+
+        $myService = app()->makewith('App\MyClasses\MyService', ['id' => $id]);
         $data = [
             'msg' => $myService->say(),
-            'data' => $myService->data()
+            'data' => $myService->alldata()
         ];
 
         return view('service.index', $data);
--- app/MyClasses/MyService.php
+++ app/MyClasses/MyService.php
@@ -5,13 +5,16 @@ use PhpParser\Builder\Class_;
 
 Class MyService
 {
-    private $msg;
-    private $data;
+    private $id = -1;
+    private $msg = 'No ID...';
+    private $data = ['hello', 'welcome', 'bye'];
 
-    public function __construct()
+    public function __construct(int $id = -1)
     {
-        $this->msg = 'Hello, this is MyService.';
-        $this->data = ['Hello', 'Welcome', 'Bye'];
+        if ($id >= 0) {
+            $this->id = $id;
+            $this->msg = 'select: ' . $this->data[$id];
+        }
     }
 
     public function say()
@@ -19,7 +22,12 @@ Class MyService
         return $this->msg;
     }
 
-    public function data()
+    public function data(int $id)
+    {
+        return $this->data[$id];
+    }
+
+    public function alldata()
     {
         return $this->data;
     }
