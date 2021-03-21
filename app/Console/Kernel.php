c8488f02b4f383613243c4f2305cc48e2a2d8edf
hidems <miura.eleven@gmail.com>
2021-03-21 18:42:54 +0100

    scheduler invoke and instance

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -6,6 +6,7 @@ use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 use App\Models\Person;
 use App\Jobs\MyJob;
+use Illuminate\Support\Facades\Storage;
 
 class Kernel extends ConsoleKernel
 {
@@ -37,11 +38,16 @@ class Kernel extends ConsoleKernel
 
         $count = Person::all()->count();
         $id = rand(0, $count) + 1;
-        $schedule->call(function() use ($id) {
-            $person = Person::find($id);
-            // Person::find($id)->delete();
-            MyJob::dispatch($person);
-        });
+
+        // Closure
+        // $schedule->call(function() use ($id) {
+        //     $person = Person::find($id);
+        //     // Person::find($id)->delete();
+        //     MyJob::dispatch($person);
+        // });
+
+        $obj = new ScheduleObj($id);
+        $schedule->call($obj);
     }
 
     /**
@@ -54,5 +60,22 @@ class Kernel extends ConsoleKernel
         $this->load(__DIR__.'/Commands');
 
         require base_path('routes/console.php');
+    }
+}
+
+Class ScheduleObj
+{
+    private $person;
+
+    public function __construct($id)
+    {
+        $this->person = Person::find($id);
+    }
+
+    public function __invoke()
+    {
+        Storage::append('person_access_log.txt', $this->person->all_data);
+        MyJob::dispatch($this->person);
+        return 'true';
     }
 }
